7da6222a0b85fe0d55415125d9b387a308ee4333
Sargis <157747247+sargisis@users.noreply.github.com>
2024-03-24 18:21:35 +0400

    Add files via upload

---
+++ Stack.cpp
@@ -0,0 +1,119 @@
+#ifndef __STACK_HPP__
+#define __STACK_HPP__
+
+#include "Stack.h"
+
+template<typename T , typename Container>
+
+stack<T,Container>::stack() 
+{} 
+
+template<typename T , typename Container>
+stack<T,Container>::stack(const stack& rhv) 
+
+{
+    ob = rhv.ob;
+}
+
+template<typename T , typename Container>
+stack<T,Container>::stack(stack&& rhv) 
+
+{
+    ob = std::move(rhv.ob);
+}
+
+template<typename T , typename Container>
+stack<T,Container>::~stack() {}
+
+template<typename T , typename Container>
+stack<T,Container>::stack(std::initializer_list<T> initlist)
+   
+{
+    for(const T& val : initlist){
+        push(val);
+    }
+}
+
+template<typename T , typename Container>
+const stack<T,Container>& stack<T,Container>::operator=(const stack& rhv){
+    if (this != &rhv){
+        
+          for (const T& val : rhv){
+              ob = rhv.ob;
+          }
+    }
+     return *this; 
+}
+
+template<typename T , typename Container>
+const stack<T,Container>& stack<T,Container>::operator=(stack&& rhv){
+        if (this != &rhv){
+
+            for (const T& val : rhv){
+                ob = std::move(rhv.ob);
+            }
+        }
+    return *this; 
+}
+template<typename T , typename Container>
+stack<T,Container>::referance stack<T,Container>::top(){
+    return Container.back();
+}
+
+template<typename T , typename Container>
+void stack<T,Container>::swap(const stack& rhv){
+     std::swap(ob , rhv.ob);
+}
+
+template<typename T , typename Container>
+bool stack<T,Container>::empty() {
+     return Container.size();
+}
+
+
+template<typename T , typename Container>
+stack<T,Container>::size_type& stack<T,Container>::size(){
+    return Container.size();
+}
+
+template<typename T , typename Container>
+void stack<T,Container>::push(const_referance elem){
+    Container.push_back(elem);
+}
+
+template<typename T , typename Container>
+void stack<T,Container>::pop(){
+    return Container.pop_back();
+}
+
+template<typename T , typename Container = std::vector<T> , typename U>
+bool operator<(const stack<T,Container> lhs, const stack<U,Container> rhs){
+    return compare(lhs, rhs) < 0;
+}
+
+template<typename T , typename Container = std::vector<T> , typename U>
+bool operator>(const stack<T,Container> lhs, const stack<U,Container> rhs){
+    return compare(lhs, rhs) > 0;
+}
+
+template<typename T , typename Container = std::vector<T> , typename U>
+bool operator<=(const stack<T,Container> lhs, const stack<U,Container> rhs){
+    return compare(lhs, rhs) <= 0;
+}
+
+template<typename T , typename Container = std::vector<T> , typename U>
+bool operator>=(const stack<T,Container> lhs, const stack<U,Container> rhs){
+    return compare(lhs, rhs) >= 0;
+}
+
+template<typename T , typename Container = std::vector<T> , typename U>
+bool operator==(const stack<T,Container> lhs, const stack<U,Container> rhs){
+    return compare(lhs, rhs) == 0;
+}
+
+template<typename T , typename Container = std::vector<T> , typename U>
+bool operator!=(const stack<T,Container> lhs, const stack<U,Container> rhs){
+    return compare(lhs, rhs) != 0;
+}
+
+#endif // 
