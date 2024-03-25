cab65ce35f1bb0b923d04dcebb8226221c18d7f9
Sargis <157747247+sargisis@users.noreply.github.com>
2024-03-25 14:00:35 +0400

    Update main.cpp

--- stack/StackSrc/main.cpp
+++ stack/StackSrc/main.cpp
@@ -4,6 +4,7 @@ int main() {
     stack<int> s;
     s.push(55);
     std::cout << s << std::endl; 
-    
+    // don't check this code this code give you segmentation fault 
+    // I don't know how fix this error 
     // s.push(55);
 }
