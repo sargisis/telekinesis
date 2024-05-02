64d75e11d1e8c64a43059740c50d125a76e4d31e
Saqo <ONLYPROGRAMMING@Sargiss-MacBook-Air.local>
2024-05-02 07:56:50 +0400

    update VT

--- Virtual Table Musican/Src/main.cpp
+++ Virtual Table Musican/Src/main.cpp
@@ -1,20 +1,6 @@
 #include "../Headers/VT.h"
 
-using namespace Sing;
-using namespace Rock;
-using namespace Drum;
-using namespace Hip;
-
 int main() {
-   Singer* ob1;
-   ob1->play();
-
-   Drummer* ob2; 
-   ob2->play();
-
-   Rocker* ob3;
-   ob3->play();
-
-   HipHop* ob4;
-   ob4->play(); 
+   Drummerist* ob; 
+   ob->vptr->play();
 }
